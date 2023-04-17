5baa57ac48d7047fff1f17473fbc291a7392a623
Kikugie <kikugie@duck.com>
2023-04-17 07:22:23 +0200

    Less modding? 🤨

--- src/main/java/me/kikugie/elytratrims/ElytraTrimsMod.java
+++ src/main/java/me/kikugie/elytratrims/ElytraTrimsMod.java
@@ -1,17 +1,17 @@
 package me.kikugie.elytratrims;
 
-import me.kikugie.elytratrims.render.ElytraTrimsManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElytraTrimsMod implements ClientModInitializer {
-
     public static final String MOD_ID = "elytratrims";
-    public static ElytraTrimsManager MANAGER;
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static Identifier id(String path) {
         return new Identifier(MOD_ID, path);
@@ -19,6 +19,7 @@ public class ElytraTrimsMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        LOGGER.info("Making elytras fancier!");
         FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(
                 id("default"), container,
                 Text.literal("Elytra Trims Defaults"),
--- src/main/java/me/kikugie/elytratrims/mixin/ElytraFeatureRendererMixin.java
+++ src/main/java/me/kikugie/elytratrims/mixin/ElytraFeatureRendererMixin.java
@@ -1,17 +1,26 @@
 package me.kikugie.elytratrims.mixin;
 
 import me.kikugie.elytratrims.access.ArmorStandEntityAccessor;
-import me.kikugie.elytratrims.render.ElytraTrimRenderer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.TexturedRenderLayers;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
 import net.minecraft.client.render.entity.model.ElytraEntityModel;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.texture.MissingSprite;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.trim.ArmorTrim;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,10 +32,16 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(ElytraFeatureRenderer.class)
 public class ElytraFeatureRendererMixin {
-    private final ElytraTrimRenderer trimRenderer = new ElytraTrimRenderer();
     @Shadow
     @Final
     private ElytraEntityModel<?> elytra;
+
+    private SpriteAtlasTexture atlas;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void saveTrimAtlas(CallbackInfo ci) {
+        atlas = MinecraftClient.getInstance().getBakedModelManager().getAtlas(TexturedRenderLayers.ARMOR_TRIMS_ATLAS_TEXTURE);
+    }
 
     @ModifyArgs(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderLayer;getArmorCutoutNoCull(Lnet/minecraft/util/Identifier;)Lnet/minecraft/client/render/RenderLayer;"))
     private void renderCapeOnGuiArmorStand(Args args, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, LivingEntity entity, float f, float g, float h, float j, float k, float l) {
@@ -42,6 +57,23 @@ public class ElytraFeatureRendererMixin {
 
     @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V"))
     private void renderElytraTrims(MatrixStack matrices, VertexConsumerProvider provider, int light, LivingEntity entity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
-        trimRenderer.renderTrim(matrices, elytra, provider, entity, entity.getEquippedStack(EquipmentSlot.CHEST), light);
+        ItemStack stack = entity.getEquippedStack(EquipmentSlot.CHEST);
+        ArmorTrim trim = ArmorTrim.getTrim(entity.world.getRegistryManager(), stack).orElse(null);
+        if (trim == null) return;
+
+        Sprite sprite = getTrimSprite(trim, atlas);
+        if (sprite.getContents().getId().equals(MissingSprite.getMissingSpriteId())) return;
+
+        VertexConsumer vertexConsumer = sprite.getTextureSpecificVertexConsumer(
+                ItemRenderer.getDirectItemGlintConsumer(provider, TexturedRenderLayers.getArmorTrims(), false, stack.hasGlint())
+        );
+
+        elytra.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1F, 1F, 1F, 1F);
+    }
+
+    private Sprite getTrimSprite(ArmorTrim trim, SpriteAtlasTexture atlas) {
+        String material = trim.getMaterial().value().assetName();
+        Identifier identifier = trim.getPattern().value().assetId().withPath(path -> "trims/models/elytra/" + path + "_" + material);
+        return atlas.getSprite(identifier);
     }
 }
