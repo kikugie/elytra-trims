a917fcfa63c6ec8af20149da4328b98c1ee9a09e
Kikugie <kikugie@duck.com>
2023-04-29 18:45:57 +0200

    Yeet some stuff

--- src/main/java/me/kikugie/elytratrims/mixin/ElytraItemMixin.java
+++ src/main/java/me/kikugie/elytratrims/mixin/ElytraItemMixin.java
@@ -1,9 +1,22 @@
 package me.kikugie.elytratrims.mixin;
 
-import net.minecraft.item.DyeableItem;
-import net.minecraft.item.ElytraItem;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.*;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.List;
+
 @Mixin(ElytraItem.class)
-public class ElytraItemMixin implements DyeableItem {
+public class ElytraItemMixin extends Item implements DyeableItem {
+    public ElytraItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        BannerItem.appendBannerTooltip(stack, tooltip);
+    }
 }
