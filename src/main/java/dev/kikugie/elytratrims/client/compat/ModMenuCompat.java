5b6414e004fc40d24e41fa1eb4efd88dbe95e498
Kikugie <kikugie@duck.com>
2024-01-15 14:54:20 +0100

    average class cast exception moment

--- src/main/java/dev/kikugie/elytratrims/client/compat/ModMenuCompat.java
+++ src/main/java/dev/kikugie/elytratrims/client/compat/ModMenuCompat.java
@@ -3,13 +3,14 @@ package dev.kikugie.elytratrims.client.compat;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.kikugie.elytratrims.client.config.ConfigScreenProvider;
+import net.minecraft.client.gui.screen.Screen;
 import org.jetbrains.annotations.Nullable;
 
 public class ModMenuCompat implements ModMenuApi {
     @Override
-    public @Nullable ConfigScreenFactory<?> getModConfigScreenFactory() {
+    public @Nullable ConfigScreenFactory<? extends Screen> getModConfigScreenFactory() {
         var provider = ConfigScreenProvider.getScreen();
-        return provider != null ? provider::apply : null;
+        return provider != null ? (ConfigScreenFactory<Screen>) provider::apply : null;
     }
 }
 /*?} */
