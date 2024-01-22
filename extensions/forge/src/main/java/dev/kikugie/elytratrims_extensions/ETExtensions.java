b5c8ca552690b4190e1dda9fc7c383e4e94a9c9e
Kikugie <kikugie@duck.com>
2024-01-22 23:30:00 +0100

    fix(extensions): Forge version crashing on startup

---
+++ extensions/forge/src/main/java/dev/kikugie/elytratrims_extensions/ETExtensions.java
@@ -0,0 +1,9 @@
+package dev.kikugie.elytratrims_extensions;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("elytratrims_extensions")
+public class ETExtensions {
+    public ETExtensions() {
+    }
+}
